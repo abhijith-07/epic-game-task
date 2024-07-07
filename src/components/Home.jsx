c490a8241c320b8febf76c9090f8200b627eea42
abhijith subash <abhijithsubash07@gmail.com>
2024-07-07 22:37:19 +0530

    fix: images added to public folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <HomeSection>
             <Heading> Epic Games : An American video game and software developer and publisher based in Cary, North Carolina. </Heading>
-            <Banner> <img src="src\assets\images\fortnite-pictures-b5kfcchwazwiz3cs 1.png" alt="" /> </Banner>
+            <Banner> <img src="images\fortnite-pictures-b5kfcchwazwiz3cs 1.png" alt="" /> </Banner>
             <Description>Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends. Find it all in Fortnite ... Drop In. </Description>
             <a href="#"><WebBtn>Visit Website </WebBtn></a>
         </HomeSection>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,15 +4,15 @@ function Projects() {
     return (
         <ProjectDiv>
             <Card>
-                <img src="src\assets\images\img1.png" alt="" />
+                <img src="images\img1.png" alt="" />
                 <p>Explore large, destructible environments where no two games are ever the same.</p>
             </Card>
             <Card>
-                <img src="src\assets\images\img2.png" alt="" />
+                <img src="images\img2.png" alt="" />
                 <p>Team up with friends by sprinting, climbing and smashing your way to earn your Victory Royale</p>
             </Card>
             <Card>
-                <img src="src\assets\images\img3.png" alt="" />
+                <img src="images\img3.png" alt="" />
                 <p>Discover even more ways to play across thousands of creator-made game genres</p>
             </Card>
         </ProjectDiv>
